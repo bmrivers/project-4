85b259cfc9f96cab73c498b9fc0bded29ad3dcd1
Bonny R <bonnyrivers@gmail.com>
2019-04-15 17:12:19 -0700

    still trying to deploy

--- src/utils/lastfmService.js
+++ src/utils/lastfmService.js
@@ -1,4 +1,4 @@
-const rootURL =  'http://ws.audioscrobbler.com/2.0/?';
+const rootURL =  'https://ws.audioscrobbler.com/2.0/?';
 const key = process.env.LASTFM_KEY ;
 
 export default function getSearchResults(track) {
